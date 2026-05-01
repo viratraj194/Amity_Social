6f038071c230f7ce0f261b7f9e0b9cfc8454ebf6
viratraj194 <121856233+viratraj194@users.noreply.github.com>
2026-05-01 18:34:10 +0530

    fix static config for compressor

--- amity_social_main/settings.py
+++ amity_social_main/settings.py
@@ -176,16 +176,25 @@ USE_I18N = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = 'static/'
+#for local development
+#STATIC_URL = 'static/'
 # for local development
 # STATIC_ROOT = BASE_DIR /'static'
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'amity_social_main/static/'),
 # ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # for production devlopment 
 
+import os
+
+STATIC_URL = '/static/'   # ✅ MUST have leading /
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),   # ✅ important
+]
 # media files 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
