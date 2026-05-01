bd0d474b37dcbf9ffb41d0361a371547a9dadff2
viratraj194 <121856233+viratraj194@users.noreply.github.com>
2026-05-01 17:53:53 +0530

    fix asgi routing import

--- amity_social_main/asgi.py
+++ amity_social_main/asgi.py
@@ -1,12 +1,16 @@
-# amity_social_main/asgi.py
-
 import os
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
+
 from django.core.asgi import get_asgi_application
-import accounts.routing
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'amity_social_main.settings')
+
+# 🔥 FIRST initialize Django
 django_asgi_app = get_asgi_application()
+
+# 🔥 THEN import routing (VERY IMPORTANT ORDER)
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import accounts.routing
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
