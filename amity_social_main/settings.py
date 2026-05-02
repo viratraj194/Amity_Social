25583511dc15c25da35c909ba61dc3fe4d1b5ca6
viratraj194 <121856233+viratraj194@users.noreply.github.com>
2026-05-02 23:33:42 +0530

    email fix

--- amity_social_main/settings.py
+++ amity_social_main/settings.py
@@ -256,13 +256,13 @@ COMPRESS_ROOT = STATIC_ROOT
 
 #Email setup
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'collage social  <collages.socialmedia@gmail.com>'
+DEFAULT_FROM_EMAIL = 'vircle  <collages.socialmedia@gmail.com>'
 
 
 ALLOWED_HOSTS = config(
