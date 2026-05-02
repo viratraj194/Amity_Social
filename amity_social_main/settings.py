5db538caa726ba04d37c4d5e445b61b2093e773a
viratraj194 <121856233+viratraj194@users.noreply.github.com>
2026-05-02 15:43:27 +0530

    updating claudinary

--- amity_social_main/settings.py
+++ amity_social_main/settings.py
@@ -221,6 +221,13 @@ STATICFILES_DIRS = [
 # media files  for local 
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_API_SECRET'),
+}
+
+
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
