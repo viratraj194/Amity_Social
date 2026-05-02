8db3d17ae8706e53c457ca780f38f93d138daf65
viratraj194 <121856233+viratraj194@users.noreply.github.com>
2026-05-02 23:10:05 +0530

    fix env loading final

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,7 +5,7 @@ from django.contrib.auth.admin import UserAdmin
 class CustomUserAdmin(UserAdmin):
     list_display = ('email', 'first_name', 'last_name', 'username', 'is_active', 'is_approved')
     ordering = ('-date_joined',)
-    list_editable = ('is_approved',)
+    list_editable = ('is_approved', 'is_active')
     filter_horizontal = ()
     list_filter = ()
     fieldsets = ()
