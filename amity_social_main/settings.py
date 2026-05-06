a00b9c0f0846139e75855fdd9f236bd3db8c5c8b
viratraj194 <121856233+viratraj194@users.noreply.github.com>
2026-05-06 15:36:32 +0530

    redis fix for railway 2

--- amity_social_main/settings.py
+++ amity_social_main/settings.py
@@ -85,7 +85,7 @@ STATICFILES_FINDERS = [
     'compressor.finders.CompressorFinder',
 ]
 COMPRESS_ENABLED = True
-COMPRESS_OFFLINE = False  # Set to True if you want to pre-compress files during deployment
+COMPRESS_OFFLINE = not DEBUG  # Set to True if you want to pre-compress files during deployment
 
 
 
