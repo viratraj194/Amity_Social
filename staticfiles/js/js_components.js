e34ae6c73731f24a1c6a040c98edb0d965e762a3
viratraj194 <121856233+viratraj194@users.noreply.github.com>
2026-05-12 11:01:48 +0530

    chenging the home page text

---
+++ staticfiles/js/js_components.js
@@ -0,0 +1,26 @@
+
+
+// postcontainer scroolbar 
+
+
+// Example of using the redirect function if needed
+
+
+
+
+
+// post card swiper 
+
+// adding api for the collage name for the profile setting
+
+
+
+// if not the profile start
+
+
+// share event to friend 
+
+
+
+
+
